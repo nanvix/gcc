3fdcb1334c4bc562400b9dd4d74c1f1633b5b8f7
Pedro Henrique Penna <ppenna@microsoft.com>
2025-03-28 15:35:46 -0700

    Dynamic Linking Support

--- gcc/config/nanvix.h
+++ gcc/config/nanvix.h
@@ -5,16 +5,19 @@
 /* Default arguments you want when running your
    i686-nanvix-gcc/x86_64-nanvix-gcc toolchain */
 #undef LIB_SPEC
-#define LIB_SPEC "-lc" /* link against C standard library */
+#define LIB_SPEC "%{!shared:-lc}" /* link against C standard library */
 
 /* Files that are linked before user code.
    The %s tells GCC to look for these files in the library directory. */
 #undef STARTFILE_SPEC
-#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+#define STARTFILE_SPEC "%{!shared:crt0.o%s} %{!shared:crti.o%s} %{!shared:crtbegin.o%s}"
 
 /* Files that are linked after user code. */
 #undef ENDFILE_SPEC
-#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+#define ENDFILE_SPEC "%{!shared:crtend.o%s} %{!shared:crtn.o%s}"
+
+#undef LINK_SPEC
+#define LINK_SPEC "%{shared:-shared} %{static:-static} %{!shared: %{!static: %{rdynamic:-export-dynamic}}}"
 
 /* Additional predefined macros. */
 #undef TARGET_OS_CPP_BUILTINS
